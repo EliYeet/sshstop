3685feb6a1ed6acfef68a7174eaf851fc860b1d9
EliYeet <68054963+EliYeet@users.noreply.github.com>
2022-12-27 17:08:14 +0100

    Add files via upload

---
+++ sshd-exec.stop.sh
@@ -0,0 +1,2 @@
+sudo systemctl stop sshd
+sudo systemctl start sshd
---
+++ sshstopper.sh
@@ -0,0 +1,4 @@
+cd /home/zaakuro/.config/systemd
+./sshd-exec.stop.sh
+zenity --info --text="SSH-Server has been stopped Status=0"
+
